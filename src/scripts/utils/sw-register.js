54ab93b43b9347f4a710e64c398a516394e1ed8d
kevinalfito69 <kevinalfito69@gmail.com>
2022-11-27 20:41:23 +0700

    Edit service worker using workbox

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,10 +1,14 @@
+import * as WorkboxWindow from 'workbox-window';
+
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
     console.log('Service worket not support in the browser');
     return;
   }
+  const wb = new WorkboxWindow.Workbox('./sw.bundle.js');
   try {
-    await navigator.serviceWorker.register('./sw.bundle.js');
+    // await navigator.serviceWorker.register('./sw.bundle.js');
+    await wb.register();
     console.log('ServiceWorker registered');
   } catch (error) {
     console.log('Failed registered serviceWorker', error);
