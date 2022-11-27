187ddaaf2f8a6c9e5ac48abc9917354ed0706cb7
kevinalfito69 <kevinalfito69@gmail.com>
2022-11-27 12:01:49 +0700

    Add cache API

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,10 +1,32 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+// Daftar asset yang akan dicaching
+
+const assetsToCache = [
+  './',
+  './icons/maskable_icon.png',
+  './icons/maskable_icon_x48.png',
+  './icons/maskable_icon_x72.png',
+  './icons/maskable_icon_x96.png',
+  './icons/maskable_icon_x128.png',
+  './icons/maskable_icon_x192.png',
+  './icons/maskable_icon_x384.png',
+  './icons/maskable_icon_x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
 self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
   console.log('installing service worker...');
 });
 self.addEventListener('activate', (event) => {
   console.log('activate service worker...');
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 self.addEventListener('fetch', (event) => {
   console.log(event.request);
-  event.respondWith(fetch(event.request));
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
